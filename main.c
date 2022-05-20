11f459f7f815948a22f90fa88373ba0ddfc90425
Michele Pagano <mpagano284@gmail.com>
2022-05-20 20:45:10 +0200

    Added SIGURS1 request + comments

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 FILE *fp;
 sem_t mutex;
@@ -11,11 +13,17 @@ int count = 0;
 void lavoro() {
     sem_wait(&mutex);
 
+    // Apri il file figli.dat in append+read mode
     fp = fopen("./figli.dat", "a+r");
+
+    // Aggiungi il pid del figlio e l'ordine di creazione
     fprintf(fp, "%d %d", getpid(), count);
     fprintf(fp, "\n");
+
+    // Chiudi il file
     fclose(fp);
 
+    // Variabile d'ordine per i figli
     count++;
 
     sem_post(&mutex);
@@ -34,12 +42,16 @@ int main() {
         if (pids[i] == 0)
             lavoro();
         else if(pids[i] > 0)
+            kill(pids[i], SIGUSR1);
             break;
     }
 
     // attendi figli
     for (int i = 0; i < 3; i++)
-
+    {
+        waitpid(pids[i], NULL, 0);
+    }
+    
     // distruggi il semaforo quando finito
     sem_destroy(&mutex);
 }
